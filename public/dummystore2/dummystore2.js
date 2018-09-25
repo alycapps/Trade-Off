83b83a52e6a4dfd0db043b2f8fad4680bbbd7d93
mwkrier <michaelwkrier@gmail.com>
2018-09-25 19:52:52 -0400

    dummystore2.html

--- public/dummystore2/dummystore2.js
+++ public/dummystore2/dummystore2.js
@@ -1,6 +1,7 @@
 $("#purchase").on('click', function(){
     $(".purchase-form").css("display", "block");
     $(".store-details").css("display", "none");
+    $(".trade-form").css("display", "none");
 });
 
 $(".stock-button").on('click', function(){
@@ -10,12 +11,27 @@ $(".stock-button").on('click', function(){
 
 $("#trade").on('click', function(){
     $(".trade-form").css("display", "block");
+    $(".purchase-form").css("display", "none");
     $(".store-details").css("display", "none");
 });
 
 $("#back").on('click', function(){
     $(".store-details").css("display", "block");
     $(".trade-form").css("display", "none");
-    $(".update-form").css("display", "block");
-    $(".purchase-form").css("display", "block");
+    $(".update-form").css("display", "none");
+    $(".purchase-form").css("display", "none");
 });
+
+function purchaseForm(){
+    let item = $('#item-number').val();
+    let amount = $('#amount').val();
+    let price = $('#price').val();
+
+    console.log("Item Number is " + item);
+    console.log("Amount equals " + amount);
+    console.log("Price is " + price);
+}
+
+$('#buy').on('click', function(){
+    purchaseForm();
+})
