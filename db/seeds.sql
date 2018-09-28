16698bfcb7e5ce51323dae78b061ace589b7aba0
unknown <38478896+nlee728@users.noreply.github.com>
2018-09-27 21:02:23 -0400

    seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,9 @@
--- User Dummy Data
-INSERT INTO users (email, password, storeName, description, createdAt, updatedAt) VALUES ("sam@email.com", "test", "Olive Garden", "All about olives", "0000-00-00 00:00:00", "0000-00-00 00:00:00");
-INSERT INTO users (email, password, storeName, description, createdAt, updatedAt) VALUES ("bill@email.com", "test", "Junk Store", "Just a bunch of my junk", "0000-00-00 00:00:00", "0000-00-00 00:00:00");
-INSERT INTO users (email, password, storeName, description, createdAt, updatedAt) VALUES ("erica@email.com", "test", "Tiny tots", "Baby items for sale", "0000-00-00 00:00:00", "0000-00-00 00:00:00");
 
+INSERT INTO users (email, password, storeName, description, createdAt, updatedAt) VALUES ("cameron@gmail.com", "$2a$10$371UxIzhwhgLrwiyShFAAuSMQsZbKZZQPT/35mx3SUxFkxX7WtZ0W", "Cam's Store", "Cool Stuff", "0000-00-00 00:00:00", "0000-00-00 00:00:00"); 
+INSERT INTO users (email, password, storeName, description, createdAt, updatedAt) VALUES ("amy@gmail.com", "$2a$10$371UxIzhwhgLrwiyShFAAuSMQsZbKZZQPT/35mx3SUxFkxX7WtZ0W", "Amy's Store", "Red Stuff","0000-00-00 00:00:00", "0000-00-00 00:00:00");
+INSERT INTO users (email, password, storeName, description, createdAt, updatedAt) VALUES ("adam@gmail.com", "$2a$10$371UxIzhwhgLrwiyShFAAuSMQsZbKZZQPT/35mx3SUxFkxX7WtZ0W", "Adam's Store","Outdoor Stuff", "0000-00-00 00:00:00", "0000-00-00 00:00:00");
 
--- Product Dummy Data
-INSERT INTO products (item, description, price, negotiable, barter, userId, createdAt, updatedAt) VALUES ("Table", "brown coffee table", "100", 0, "0", 1, "0000-00-00 00:00:00", "0000-00-00 00:00:00");
-INSERT INTO products (item, description, price, negotiable, barter, userId, createdAt, updatedAt) VALUES ("Chair", "yellow chair", "10", 0, "0", 1, "0000-00-00 00:00:00", "0000-00-00 00:00:00");
-INSERT INTO products (item, description, price, negotiable, barter, userId, createdAt, updatedAt) VALUES ("Laptop", "Dell laptop", "500", 0, "0", 2, "0000-00-00 00:00:00", "0000-00-00 00:00:00");
-INSERT INTO products (item, description, price, negotiable, barter, userId, createdAt, updatedAt) VALUES ("Picture Frame", "8x10 silver frame", "2", 0, "0", 2, "0000-00-00 00:00:00", "0000-00-00 00:00:00"); 
-INSERT INTO products (item, description, price, negotiable, barter, userId, createdAt, updatedAt) VALUES ("Picture Frame", "8x10 silver frame", "2", 0, "0", 4, "0000-00-00 00:00:00", "0000-00-00 00:00:00"); 
+INSERT INTO products (item, description, price, userId, createdAt, updatedAt) VALUES ("Table", "brown coffee table", "10", 1, "0000-00-00 00:00:00", "0000-00-00 00:00:00");
+INSERT INTO products (item, description, price, userId, createdAt, updatedAt) VALUES ("Chairs", "Set of 4 chairs", "60", 1, "0000-00-00 00:00:00", "0000-00-00 00:00:00");
+INSERT INTO products (item, description, price, userId, createdAt, updatedAt) VALUES ("Laptop", "Dell Inspiron", "340", 1, "0000-00-00 00:00:00", "0000-00-00 00:00:00");
+INSERT INTO products (item, description, price, userId, createdAt, updatedAt) VALUES ("Paper", "extra loose leaf paper, college ruled", "1", 1, "0000-00-00 00:00:00", "0000-00-00 00:00:00");
